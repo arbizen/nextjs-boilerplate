b60b4a1d23967a057d2c23608c92d06ceaffe27c
Arb Rahim Badsa <arbrahimbadsa@gmail.com>
2024-05-23 15:59:54 +0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return <div>Hello world</div>;
+import axios from 'axios';
+import { cookies } from 'next/headers';
+
+export default async function Page() {
+  const sessionId = cookies().get('sessionId')?.value;
+  console.log('Session ID:', sessionId);
+  const res = await axios.get(
+    `https://backend.recovo.me/api/products/get-all-group-sku-by-slug/en/mostaza-algod%C3%B3n-french-terry`,
+    { headers: { sessionId: sessionId } },
+  );
+  const data = await res.data;
+  return <div>{JSON.stringify(data)}</div>;
 }
