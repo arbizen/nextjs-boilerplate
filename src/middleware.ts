c73b91b6006a2fa3d61906c1e86cd8d102ccb680
Arb Rahim Badsa <arbrahimbadsa@gmail.com>
2024-05-23 15:24:33 +0600

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,53 +3,72 @@ import type { NextRequest } from 'next/server';
 import { cookies } from 'next/headers';
 import { v4 as uuidv4 } from 'uuid';
 
-import {
-  ResponseCookies,
-  RequestCookies,
-} from 'next/dist/server/web/spec-extension/cookies';
+// import {
+//   ResponseCookies,
+//   RequestCookies,
+// } from 'next/dist/server/web/spec-extension/cookies';
 
-function applySetCookie(req: NextRequest, res: NextResponse) {
-  // 1. Parse Set-Cookie header from the response
-  const setCookies = new ResponseCookies(res.headers);
+// function applySetCookie(req: NextRequest, res: NextResponse) {
+//   // 1. Parse Set-Cookie header from the response
+//   const setCookies = new ResponseCookies(res.headers);
 
-  // 2. Construct updated Cookie header for the request
-  const newReqHeaders = new Headers(req.headers);
-  const newReqCookies = new RequestCookies(newReqHeaders);
-  setCookies.getAll().forEach((cookie) => newReqCookies.set(cookie));
+//   // 2. Construct updated Cookie header for the request
+//   const newReqHeaders = new Headers(req.headers);
+//   const newReqCookies = new RequestCookies(newReqHeaders);
+//   setCookies.getAll().forEach((cookie) => newReqCookies.set(cookie));
 
-  // 3. Set up the “request header overrides” (see https://github.com/vercel/next.js/pull/41380)
-  //    on a dummy response
-  // NextResponse.next will set x-middleware-override-headers / x-middleware-request-* headers
-  const dummyRes = NextResponse.next({ request: { headers: newReqHeaders } });
+//   // 3. Set up the “request header overrides” (see https://github.com/vercel/next.js/pull/41380)
+//   //    on a dummy response
+//   // NextResponse.next will set x-middleware-override-headers / x-middleware-request-* headers
+//   const dummyRes = NextResponse.next({ request: { headers: newReqHeaders } });
 
-  // 4. Copy the “request header overrides” headers from our dummy response to the real response
-  dummyRes.headers.forEach((value, key) => {
-    if (
-      key === 'x-middleware-override-headers' ||
-      key.startsWith('x-middleware-request-')
-    ) {
-      res.headers.set(key, value);
-    }
-  });
-}
+//   // 4. Copy the “request header overrides” headers from our dummy response to the real response
+//   dummyRes.headers.forEach((value, key) => {
+//     if (
+//       key === 'x-middleware-override-headers' ||
+//       key.startsWith('x-middleware-request-')
+//     ) {
+//       res.headers.set(key, value);
+//     }
+//   });
+// }
 
 export function middleware(request: NextRequest) {
-  const res = NextResponse.next();
+  console.log('Request for : ', request.nextUrl.pathname);
+  // const res = NextResponse.next();
 
-  const sessionId = cookies().get('sessionId')?.value;
-  if (!sessionId) {
+  // const sessionId = cookies().get('sessionId')?.value;
+  // if (!sessionId) {
+  //   const randomSessionId = uuidv4();
+  //   console.log('Session not found.', 'Setting session ID:', randomSessionId);
+  //   res.cookies.set('sessionId', randomSessionId, {
+  //     path: '/',
+  //     expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365 * 5), // 5 years
+  //   });
+  //   applySetCookie(request, res);
+  // } else {
+  //   console.log('Session ID:', sessionId);
+  // }
+
+  // return res;
+
+  const response = NextResponse.next();
+
+  const requestCookie = request.cookies.get('sessionId')?.value;
+
+  if (!requestCookie) {
     const randomSessionId = uuidv4();
-    console.log('Session not found.', 'Setting session ID:', randomSessionId);
-    res.cookies.set('sessionId', randomSessionId, {
+    response.cookies.set({
+      name: 'sessionId',
+      value: randomSessionId,
       path: '/',
-      expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365 * 5), // 5 years
     });
-    applySetCookie(request, res);
+    console.log('Session not found.', 'Setting session ID:', randomSessionId);
   } else {
-    console.log('Session ID:', sessionId);
+    console.log('Session ID:', requestCookie);
   }
 
-  return res;
+  return response;
 }
 
 export const config = {
@@ -62,5 +81,6 @@ export const config = {
      * - favicon.ico (favicon file)
      */
     '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    //'/',
   ],
 };
