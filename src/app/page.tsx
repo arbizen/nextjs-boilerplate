79cb6efeb796aa830f22edbb6d7ba46aa9f90d3b
Arb Rahim Badsa <34975329+arbizen@users.noreply.github.com>
2024-06-18 13:49:25 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,20 +28,25 @@ export default async function Page() {
   );
   const data = await res.json();
   return (
-    <div>
-      <div>Session </div>
-      <div>
-        SessionId: {sessionId}
-        <br />
-        UserId: {userId}
-        <br />
-        Event: {event}
-        <br />
-        Path Origin: {pathOrigin}
-        <br />
-        Language: {lang}
-        <br />
-      </div>
-    </div>
+    <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
   );
 }
